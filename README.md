5f90074f92b1818ef8a864ac5c84e82ccb1514f0
gymagyel <227876535+gymagyel@users.noreply.github.com>
2026-01-23 13:09:22 +0000

    Set up Book constructor and display books as cards

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# book
+# Library App
+
+A small JavaScript project to practice:
+- constructors
+- prototypes
+- arrays
+- DOM manipulation
+
+## Current progress
+- [x] Project setup
+- [ ] Book constructor
+- [ ] Library array
+- [ ] Display books
+- [ ] Add/remove books
--- script.js
+++ script.js
@@ -1,13 +1,37 @@
+
+const myLibrary = []
 function Book(title,author,pages,read) {
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-};
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '293', 'not read yet')
+    function addBookToLibrary(title,author, pages, read) {
+const book = new Book (title,author,pages,read);
+myLibrary.push(book);
+    }
 
-console.log(theHobbit.info());
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 293, false)
+addBookToLibrary("Dune", "Frank Herbert", 412, true)
+
+function displayLibrary() {
+    const libraryDiv = document.getElementById("library");
+    libraryDiv.innerHTML="";
+
+myLibrary.forEach(book =>{
+    const card = document.createElement("div");
+
+    card.classList.add("card")
+    card.textContent = 
+    `${book.title} 
+    ${book.author}`
+    libraryDiv.appendChild(card);
+})
+}
+displayLibrary();
